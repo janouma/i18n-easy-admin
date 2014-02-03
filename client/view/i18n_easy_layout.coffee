2b1663f32e069fd0bfcbf3f6a7b257641f4b5c02
Judicaël Anouma <janouma@gmail.com>
2014-02-03 20:04:58 +0100

    Get all necessary files from i18n-easy package

---
+++ client/view/i18n_easy_layout.coffee
@@ -0,0 +1,3 @@
+Template.i18n_easy_layout.events {
+	'click .i18n-easy': (e, template)-> $(template.find '.upload-form').addClass 'hidden'
+}
---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+	summary: "Admin ui for i18n-easy"
+});
+
+Package.on_use(function(api, where){
+	api.use(['coffeescript', 'minimongo', 'mongo-livedata', 'iron-router']);
+	api.use(['templating', 'handlebars', 'font-awesome'], 'client');
+
+	var clientFiles = [];
+
+	clientFiles.push('i18n_easy_router.coffee');
+	clientFiles.push('client/helpers/alert.coffee');
+	clientFiles.push('client/view/i18n_easy_side_nav.html');
+	clientFiles.push('client/view/i18n_easy_side_nav.coffee');
+	clientFiles.push('client/view/i18n_easy_nav.html');
+	clientFiles.push('client/view/i18n_easy_nav.coffee');
+	clientFiles.push('client/view/i18n_easy_header.html');
+	clientFiles.push('client/view/i18n_easy_footer.html');
+	clientFiles.push('client/view/i18n_easy_layout.html');
+	clientFiles.push('client/view/i18n_easy_layout.coffee');
+	clientFiles.push('client/view/i18n_easy_translation.html');
+	clientFiles.push('client/view/i18n_easy_translation.coffee');
+	clientFiles.push('client/view/i18n_easy_admin.html');
+	clientFiles.push('client/view/i18n_easy_admin.coffee');
+	clientFiles.push('client/view/i18n_easy_forbidden.html');
+	clientFiles.push('client/stylesheets/style.css');
+	clientFiles.push('client/stylesheets/build-full-no-icons.min.css');
+
+	api.add_files(clientFiles, 'client');
+	api.add_files(['server/i18n_easy_init.coffee', 'i18n_easy_router.coffee'], 'server');
+});
